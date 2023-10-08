378fc54ad26aa6ba8378ed974d56f1eae42ec513
Andres Rios Bolivar <andresriosbolivar27@gmail.com>
2023-10-08 17:19:06 -0500

    Implement Aapplication Program

---
+++ Entrega2_Patrones/Program.cs
@@ -0,0 +1,13 @@
+﻿using Entrega2_Patrones.Bridges.Implements;
+using Entrega2_Patrones.Facades;
+
+public class Program
+{
+    public static void Main()
+    {
+        var facade = new ServiceCoreFacade();
+        facade.ProcessNewShipping("Nuevo envío disponible", new EmailNotification());
+
+        facade.ProcessOldShipping("Envío antiguo procesado", new SMSNotification());
+    }
+}
