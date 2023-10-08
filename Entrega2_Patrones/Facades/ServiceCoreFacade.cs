b2d1f14c5c1e72e4002f5664acf98ff0bf089802
Andres Rios Bolivar <andresriosbolivar27@gmail.com>
2023-10-08 17:18:04 -0500

    Implement Facade Pattern

---
+++ Entrega2_Patrones/Facades/ServiceCoreFacade.cs
@@ -0,0 +1,33 @@
+﻿using Entrega2_Patrones.Adapters.Abstractions;
+using Entrega2_Patrones.Adapters.Implements;
+using Entrega2_Patrones.Bridges;
+using Entrega2_Patrones.Bridges.Abstractions;
+using Entrega2_Patrones.Business.Workflows;
+
+namespace Entrega2_Patrones.Facades
+{
+    public class ServiceCoreFacade
+    {
+        private readonly IOldShippingService oldShippingService;
+        private readonly INewShippingService newShippingService;
+
+        public ServiceCoreFacade()
+        {
+            this.oldShippingService = new OldShippingService();
+            this.newShippingService = new NewShippingService();
+        }
+
+        public void ProcessNewShipping(string message, INotificationImplementor implementor)
+        {
+            new SenderWF(this.oldShippingService, this.newShippingService).ProcessNewShipping();
+
+            new ShipmentNotificationWF(implementor).Notify(message);
+        }
+
+        public void ProcessOldShipping(string message, INotificationImplementor implementor)
+        {
+            new SenderWF(this.oldShippingService, this.newShippingService).ProcessOldShipping();
+            new ShipmentNotificationWF(implementor).Notify(message);
+        }
+    }
+}
